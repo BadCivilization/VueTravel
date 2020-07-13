4583bbe2eaf079c76ea4b42c1b387b332cfd0fb9
BadCivilization <739682923@qq.com>
2020-07-13 20:04:38 +0800

    add header

---
+++ src/pages/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <header-home>
+    </header-home>
+  </div>
+</template>
+
+<script>
+import HeaderHome from './home/components/header'
+export default {
+  name: 'Home',
+  components: {
+    HeaderHome
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/home/components/header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+        <span class="iconfont back-header">&#xe697;</span>
+    </div>
+    <div class="header-input iconfont">&#xe6ac;输入城市/景点/游玩主题</div>
+    <div class="header-right">城市
+        <span class="iconfont arrow-header">&#xe62a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderHome'
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~@/assets/styles/var.styl'
+    .header{
+        background-color: $bgColor;
+        color: #ffffff;
+        display: flex;
+        line-height: .86rem;
+    }
+    .header-left{
+        width: .64rem;
+        float: left;
+    }
+    .header-left .back-header{
+        display: block;
+        font-size: .6rem;
+        text-align: center;
+    }
+    .header-input{
+        height: .64rem;
+        line-height: .64rem;
+        margin-top: .11rem;
+        margin-left: .2rem
+        padding-left .2rem
+        flex: 1;
+        background: #ffffff;
+        border-radius: 5px;
+        color: #ccc;
+    }
+    .header-right{
+        width: 1.24rem;
+        float: right;
+        text-align: center;
+    }
+    .header-right .arrow-header{
+        font-size: .3rem;
+    }
+</style>
