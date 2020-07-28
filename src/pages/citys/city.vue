b24baf92990da567aa91fa8fa1e9ca3ad72197a7
BadCivilization <739682923@qq.com>
2020-07-28 17:31:59 +0800

    city list start

--- src/pages/citys/city.vue
+++ src/pages/citys/city.vue
@@ -1,14 +1,19 @@
 <template>
+  <div>
     <city-header></city-header>
+    <city-list></city-list>
+  </div>
 </template>
 
 <script>
 import CityHeader from './components/header'
+import CityList from './components/list'
 
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CityList
   }
 }
 </script>
