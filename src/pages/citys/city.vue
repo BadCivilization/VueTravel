cd8f5568216ef6fe35be64978f41bdce4f361f81
BadCivilization <739682923@qq.com>
2020-07-29 14:53:50 +0800

    listlayout

--- src/pages/citys/city.vue
+++ src/pages/citys/city.vue
@@ -1,34 +1,26 @@
 <template>
-<<<<<<< HEAD
   <div>
     <city-header></city-header>
+    <city-search></city-search>
     <city-list></city-list>
+    <alpha-bet></alpha-bet>
   </div>
-=======
-    <div>
-        <city-header></city-header>
-        <city-search></city-search>
-    </div>
->>>>>>> 96190514105fbe9c55263015ed9588a96a5c64d0
+
 </template>
 
 <script>
 import CityHeader from './components/header'
-<<<<<<< HEAD
 import CityList from './components/list'
-
-=======
 import CitySearch from './components/search'
->>>>>>> 96190514105fbe9c55263015ed9588a96a5c64d0
+import AlphaBet from './components/alphabet'
+
 export default {
   name: 'City',
   components: {
     CityHeader,
-<<<<<<< HEAD
-    CityList
-=======
-    CitySearch
->>>>>>> 96190514105fbe9c55263015ed9588a96a5c64d0
+    CityList,
+    CitySearch,
+    AlphaBet
   }
 }
 </script>
