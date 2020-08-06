46ce4fb625aa515a23a73de3608f1335d57a1b25
BadCivilization <739682923@qq.com>
2020-08-06 20:09:13 +0800

    header渐隐渐显

---
+++ src/pages/detail/components/head.vue
@@ -0,0 +1,96 @@
+<template>
+    <div>
+        <router-link class="header-abs" tag="div" to="/" v-show="showabs">
+            <span class="iconfont back-header">&#xe697;</span>
+        </router-link>
+        <div class="header-fixed"  v-show="!showabs" :style="opacityStyle">
+            <div class="header">
+                <div class="header-left">
+                    <router-link class="iconfont back-header" to="/">&#xe697;</router-link>
+                </div>
+            <div class="header-txt">输入城市/景点/游玩主题</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailHeader',
+  data: function () {
+    return {
+      showabs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    windowScroll: function () {
+      const sTop = document.documentElement.scrollTop
+      if (sTop > 0) {
+        let opa = sTop / 100
+        opa = opa > 1 ? 1 : opa
+        this.opacityStyle = {
+          opacity: opa
+        }
+        this.showabs = false
+      } else {
+        this.showabs = true
+      }
+    }
+  },
+  activated: function () {
+    window.addEventListener('scroll', this.windowScroll)
+  }
+}
+
+</script>
+
+<style scoped lang="stylus">
+    @import '~@/assets/styles/var.styl'
+    @import '~@/assets/styles/mixin.styl'
+    .header-abs{
+        position absolute
+        left .2rem
+        top .2rem
+        border-radius .4rem
+        width .8rem
+        height .8rem
+        background-color rgba(0, 0, 0, 0.5)
+    }
+    .header-fixed{
+        position fixed
+        top 0
+        left 0
+        right 0
+    }
+    .back-header{
+        color #fff
+        font-size .5rem
+        line-height .8rem
+        margin-left .13rem
+    }
+
+    .header{
+        position relative
+        background-color: $bgColor;
+        color: #ffffff;
+        display: flex;
+        line-height: $headerHeight;
+    }
+    .header-left{
+        width: .64rem;
+        position absolute
+    }
+    .header-left .back-header{
+        display: block;
+        font-size: .6rem;
+        text-align: center;
+    }
+    .header-txt{
+        flex 1
+        text-align center
+    }
+
+</style>
--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -1,19 +1,25 @@
 <template>
     <div>
         <detail-banner></detail-banner>
+        <detail-header></detail-header>
+        <div class="con"></div>
     </div>
 </template>
 
 <script>
 import DetailBanner from './components/banner'
+import DetailHeader from './components/head'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner: DetailBanner
+    DetailBanner: DetailBanner,
+    DetailHeader: DetailHeader
   }
 }
 </script>
 
 <style scoped lang="stylus">
-
+  .con{
+    height 30rem
+  }
 </style>
