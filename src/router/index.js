dfe88604b23a62c9c5ac7757a6eee0cdfe115986
BadCivilization <739682923@qq.com>
2020-08-12 15:01:05 +0800

    finish

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../pages/home.vue'
-import City from '../pages/citys/city.vue'
-import Detail from '../pages/detail/detail.vue'
 
 Vue.use(Router)
+
+// 解决ElementUI导航栏中的vue-router在3.0版本以上重复点菜单报错问题
+const originalPush = Router.prototype.push
+Router.prototype.push = function push (location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: () => import('../pages/home.vue')
     }, {
       path: '/city',
       name: 'City',
-      component: City
+      component: () => import('../pages/citys/city.vue')
     }, {
       path: '/detail/:id',
       name: 'Detail',
-      component: Detail
+      component: () => import('../pages/detail/detail.vue')
     }
   ],
   scrollBehavior (to, from, savedPosition) {
